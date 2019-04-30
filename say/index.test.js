4d8ed4878a868e7e8fc7681d9f39c45a46ea6b99
Maria Pia Natale <mariapia.natale@studenti.unitn.it>
2019-04-30 12:23:42 +0200

    added more tests

--- say/index.test.js
+++ say/index.test.js
@@ -4,3 +4,19 @@ import { translate } from './index'
 test('should return zero', t => {
   t.is(translate(0), 'zero')
 })
+
+test('should return one', t => {
+  t.is(translate(1), 'one')
+})
+
+test('should return two', t => {
+  t.is(translate(2), 'two')
+})
+
+test('should return three', t => {
+  t.is(translate(3), 'three')
+})
+
+test('should return four', t => {
+  t.is(translate(4), 'four')
+})
