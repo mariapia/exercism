6f2a881490d1539e1e26a442dc22f5989e4ce22c
Maria Pia Natale <mariapia.natale@studenti.unitn.it>
2019-05-03 10:06:52 +0200

    added operations

--- accumulate/index.js
+++ accumulate/index.js
@@ -1,18 +1,23 @@
 const accumulate = (list, operation) => {
-  const square = (list) => {
-    const squaredList = list.map(num => num * num)
-    return squaredList
-  }
-
-  const increment = (list) => {
-    return list.map(num => num++)
-  }
-
   const operations = {
     'square': square(list),
-    'increment': increment(list)
+    'increment': increment(list),
+    'reverse': reverse(list)
   }
 
+  return operations[operation]
+}
+
+function square (list) {
+  return list.map(num => num * num)
+}
+
+function increment (list) {
+  return list.map(num => num + 1)
+}
+
+function reverse (list) {
+  return list.map(word => word.toString().split('').reverse().join(''))
 }
 
 module.exports = { accumulate }
--- accumulate/index.test.js
+++ accumulate/index.test.js
@@ -3,5 +3,18 @@ import { accumulate } from './index'
 
 test('should square the numbers of the list', t => {
   const list = [2]
-  t.is(accumulate(list, 'square'), [4])
+  const squared = [4]
+  t.deepEqual(accumulate(list, 'square'), squared)
+})
+
+test('should increment the numbers of the list', t => {
+  const list = [4]
+  const incremented = [5]
+  t.deepEqual(accumulate(list, 'increment'), incremented)
+})
+
+test('should return the reversed string of the list', t => {
+  const list = ['hello', 'how', 'are', 'you', '?']
+  const reversed = ['olleh', 'woh', 'era', 'uoy', '?']
+  t.deepEqual(accumulate(list, 'reverse'), reversed)
 })
